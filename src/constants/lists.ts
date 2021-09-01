36686608aa886bf71261a6c735a8e22b322769ac
Chengdujun <53704612+Chengdujun@users.noreply.github.com>
2021-09-01 08:14:10 +0800

    git push -u origin main

---
+++ src/constants/lists.ts
@@ -0,0 +1,15 @@
+// the Uniswap Default token list lives here
+//export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
+
+//export const DEFAULT_LIST_OF_LISTS: string[] = [
+ // DEFAULT_TOKEN_LIST_URL 
+//]
+
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'http://www.hecc.club/qfy-content/uploads/2021/08/acb4dc831c0f9fb0acfc38718b78e9db.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  
+ 
+]
